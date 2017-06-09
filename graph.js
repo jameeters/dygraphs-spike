fda22c6894fa7b4eb9ac4c362b79b993e3ce5ec2
James McFeeters <jameeters@gmail.com>
2017-06-09 15:12:47 -0500

    Make minor refinements to style and enlarge graph.
    Change default roll period to 10, delete the graph without any rolling
    average, and set the remaining graph to occupy the entire page.

--- graph.js
+++ graph.js
@@ -1,11 +1,9 @@
-g1 = new Dygraph(document.getElementById("graphdiv1"), "test_data2.csv", {});
-
-g2 = new Dygraph(
-	document.getElementById("graphdiv2"),
+g = new Dygraph(
+	document.getElementById("graphdiv"),
 	"test_data2.csv",
 	{
 		showRoller: true,
-		rollPeriod: 3
+		rollPeriod: 10
 	}
 );
 
